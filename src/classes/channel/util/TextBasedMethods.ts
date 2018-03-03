04d113bb51a01b91d8b712c824e10b6ef1a35620
EricRabil <ericjrabil@gmail.com>
2018-03-03 09:58:19 -0500

    Properly catch errors when sending a message

--- src/classes/channel/util/TextBasedMethods.ts
+++ src/classes/channel/util/TextBasedMethods.ts
@@ -7,9 +7,9 @@ import { sendMessage as sendRawMessage } from "../../../util/rest/actions/Channe
 export namespace TextBasedMethods {
     export function sendMessage(message: SendableMessage, channel: string): Promise<MessageRecord> {
         const nonce = createNonce();
-        return new Promise((resolve) => {
-            message.nonce = nonce;
-            sendRawMessage(message, channel);
+        message.nonce = nonce;
+        return new Promise((resolve, reject) => {
+            sendRawMessage(message, channel).catch(reject);
             resolve();
         }).then(() => MessageStore.once(nonce));
     }

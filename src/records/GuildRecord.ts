e19ee7c9b8a681fbad8f34e72ebfe06c1e5fe77a
EricRabil <ericjrabil@gmail.com>
2018-03-03 10:03:01 -0500

    Added all actions to guild

--- src/records/GuildRecord.ts
+++ src/records/GuildRecord.ts
@@ -6,13 +6,19 @@ import { RawGuildMember } from "../types/raw/RawGuildMember";
 import { RawChannel } from "../types/raw/RawChannel";
 import { Presence } from "../types/discord/user/presence";
 import { GuildMemberRecord } from "./GuildMemberRecord";
-import { GuildMemberStore } from "../stores/index";
+import { GuildMemberStore, mixedMemberInsert, handleGuildMemberAddOrUpdate } from "../stores/index";
 import { ChannelRecord } from "./ChannelRecord";
 import { ChannelStore } from "../stores/ChannelStore";
 import { TextChannel } from "../classes/channel/TextChannel";
 import { VoiceChannel } from "../classes/channel/VoiceChannel";
 import { ChannelCategory } from "../classes/channel/ChannelCategory";
 import { ChannelTypes } from "../util/Constants";
+import { deleteGuild, ChannelCreate, createGuild, createGuildChannel, ChannelEdit, modifyChannelPositions, GuildMembersQuery, getMembers, AddGuildMemberRequest, addMember, EditGuildMemberRequest, editMember, setSelfNickname, getBans, removeBan, RolePosition, modifyRolePositions, getPruneCount, pruneMembers, getVoiceRegions, getInvites, getGuildEmbed, editGuildEmbed, getVanityUrl } from "../util/rest/actions/GuildActions";
+import { RawUser, RawBan, RawVoiceRegion, RawIntegration, RawGuildEmbed } from "../types/raw";
+import { BanRecord } from "./BanRecord";
+import { RoleRecord } from "./RoleRecord";
+import { RoleStore } from "../stores/RoleStore";
+import { InviteRecord } from "./InviteRecord";
 
 export class GuildRecord extends Record implements RawGuild {
 
@@ -31,7 +37,7 @@ export class GuildRecord extends Record implements RawGuild {
     verification_level: number;
     default_message_notifications: number;
     explicit_content_filter: number;
-    roles: RawRole[];
+    roles: RoleRecord[];
     emojis: RawEmoji[];
     features: string[];
     mfa_level: number;
@@ -54,6 +60,7 @@ export class GuildRecord extends Record implements RawGuild {
         this.assign(data, ["members", "channels"]);
         this.readonly("channels", () => Array.from(this.channelsMapped.values()));
         this.readonly("members", () => Array.from(GuildMemberStore.membersFor(this).values()));
+        this.readonly("roles", () => Array.from(this.roleMap.values()));
     }
 
     /**
@@ -90,4 +97,89 @@ export class GuildRecord extends Record implements RawGuild {
     public get channelCategories(): Map<string, ChannelCategory> {
         return ChannelStore.getTypedChannelsForGuild(this, ChannelTypes.CATEGORY) as any;
     }
+
+    /**
+     * Deletes the guild
+    */
+    public deleteGuild(): Promise<void> {
+        return deleteGuild(this.id);
+    }
+
+    /**
+     * Creates a channel for the given guild
+     * @param channel the channel create request
+     */
+    public async createChannel(channel: ChannelCreate): Promise<ChannelRecord> {
+        const {id} = await createGuildChannel(this.id, channel);
+        return await ChannelStore.once(id);
+    }
+
+    public async modifyChannelPositions(positions: ChannelEdit): Promise<void> {
+        return modifyChannelPositions(this.id, positions);
+    }
+
+    public getMember(id: string): Promise<GuildMemberRecord | undefined> {
+        return GuildMemberStore.findOrCreate(id, this.id);
+    }
+
+    public async getMembers(parameters?: GuildMembersQuery): Promise<Map<string, GuildMemberRecord>> {
+        return mixedMemberInsert(this.id, await getMembers(this.id, parameters));
+    }
+
+    public async addMember(user: string | RawUser, oauthToken: string, opts: AddGuildMemberRequest): Promise<GuildMemberRecord> {
+        return handleGuildMemberAddOrUpdate(await addMember(this.id, typeof user === "string" ? user : user.id, oauthToken, opts)) as GuildMemberRecord;
+    }
+
+    public async getBans(): Promise<Map<string, RawBan>> {
+        const bans: Map<string, BanRecord> = new Map();
+        for (const ban of await getBans(this.id)) {
+            bans.set(ban.user.id, new BanRecord(ban, this));
+        }
+        return bans;
+    }
+
+    public unban(user: string): Promise<void> {
+        return removeBan(this.id, user);
+    }
+
+    public get roleMap(): Map<string, RoleRecord> {
+        return RoleStore.getOrCreateSection(this.id);
+    }
+
+    public modifyRolePositions(positions: RolePosition[]): Promise<void> {
+        return modifyRolePositions(this.id, positions) as Promise<any>;
+    }
+
+    public getPruneCount(inactivityDays: number = 1): Promise<number> {
+        return getPruneCount(this.id, inactivityDays);
+    }
+
+    public pruneMembers(inactivityDays: number = 1): Promise<number> {
+        return pruneMembers(this.id, inactivityDays);
+    }
+
+    public getVoiceRegions(): Promise<RawVoiceRegion[]> {
+        return getVoiceRegions(this.id);
+    }
+
+    public async getInvites(): Promise<InviteRecord[]> {
+        const invites: InviteRecord[] = [];
+        for (const invite of await getInvites(this.id)) {
+            invites.push(new InviteRecord(invite));
+        }
+        return invites;
+    }
+
+    public getEmbed(): Promise<RawGuildEmbed> {
+        return getGuildEmbed(this.id);
+    }
+
+    public editEmbed(embed: RawGuildEmbed): Promise<RawGuildEmbed> {
+        return editGuildEmbed(this.id, embed);
+    }
+
+    public getVanityURL(): Promise<string> {
+        return getVanityUrl(this.id);
+    }
+
 }
